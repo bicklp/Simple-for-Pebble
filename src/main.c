59a2b05f96bff4057a5fe763a214ac4216c1222a
bicklp <Phil@philips-mbp.home>
2014-09-01 13:00:49 +0100

    Revert 46fda87..7bd3fb8
    This rolls back to commit 46fda87a6e0e8cbfb9519cc92ed0057fc5a35db9.

--- src/main.c
+++ src/main.c
@@ -49,6 +49,7 @@ void set_day_layer(int day)
     case 5: startX = 80; break;
     case 6: startX = 95; break;
     case 0: startX = 110; break;
+    case 10: startX = 110; break;
   }
   // Create Layer
   
